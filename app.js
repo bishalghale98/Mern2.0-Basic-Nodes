3023c31648cac3bbf2c52532d16021a70c5f3cbe
bishalghale98 <bishalghle@gmail.com>
2025-01-16 17:16:20 +0545

    add book model and endpoint for adding books complete create operation for books

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
 const ConnectToDatabase = require("./database/index");
+const Book = require("./models/bookModel");
 
 // const app = require("express")(); alternative way to import express
+
+app.use(express.json());
 
 ConnectToDatabase();
 
@@ -10,6 +13,30 @@ app.get("/", (req, res) => {
   res.status(200).json({
     message: "Hello World",
     name: "John Doe",
+  });
+});
+
+// for create books
+
+app.post("/add-book", async (req, res) => {
+  const {
+    bookName,
+    bookPrice,
+    isbnNumber,
+    authorName,
+    publishedAt,
+    publication,
+  } = req.body;
+  await Book.create({
+    bookName,
+    bookPrice,
+    isbnNumber,
+    authorName,
+    publishedAt,
+    publication,
+  });
+  res.status(201).json({
+    message: "Book added successfully",
   });
 });
 
--- database/index.js
+++ database/index.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
 
 const ConnectionString =
-  "mongodb+srv://bishal:bishalghale@cluster0.kj5af.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+  "mongodb+srv://bishal:123@cluster0.kj5af.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 async function ConnectToDatabase() {
   await mongoose.connect(ConnectionString);
   console.log("Connected to Database successfully");
 }
+
 
 module.exports = ConnectToDatabase;
