e6aacd0609d38e12b53ca83522aedae2c96cbee6
bishalghale98 <bishalghle@gmail.com>
2025-04-12 07:39:59 +0545

    update database connection string for improved security and access

--- database/index.js
+++ database/index.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const ConnectionString =
-  "mongodb+srv://bishal:3CE2kTFY0WtbOMeL@mern.kj5af.mongodb.net/?retryWrites=true&w=majority&appName=MERN";
+  "mongodb+srv://mern:mern2.0@cluster0.tfply74.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 async function ConnectToDatabase() {
   await mongoose.connect(ConnectionString);
