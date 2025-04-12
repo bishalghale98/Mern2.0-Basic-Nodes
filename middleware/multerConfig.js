3fca7ac972c213474724f344d1f985b9dc15424d
bishalghale98 <bishalghle@gmail.com>
2025-04-12 08:47:46 +0545

    add file type validation to multer storage configuration

--- middleware/multerConfig.js
+++ middleware/multerConfig.js
@@ -2,7 +2,12 @@ const multer = require("multer");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./storage"); // cb (error, success)
+    const allowedFileTypes = ["image/png", "image/jpg", "image/jpeg"];
+  if(!allowedFileTypes.includes(file.mimetype)){
+    cb(new Error("File type is not supported")) // --> cb (error)
+    return
+  }
+    cb(null, "./storage"); // --> cb (error, success)
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + "-" + file.originalname);
