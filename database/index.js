8ca308ed0c0cca97f6039d1b28ab203b245d5e2e
bishalghale98 <bishalghle@gmail.com>
2025-04-13 19:58:19 +0545

    update database connection string and add error handling for connection failures

--- database/index.js
+++ database/index.js
@@ -1,12 +1,14 @@
 const mongoose = require("mongoose");
 
-const ConnectionString =
-  "mongodb+srv://mern:mern2.0@cluster0.tfply74.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const ConnectionString = "mongodb+srv://ghale1040:ghalevai@cluster0.tfply74.mongodb.net/yourDatabaseName?retryWrites=true&w=majority&appName=Cluster0";
 
 async function ConnectToDatabase() {
-  await mongoose.connect(ConnectionString);
-  console.log("Connected to Database successfully");
+  try {
+    await mongoose.connect(ConnectionString);
+    console.log("Connected to Database successfully");
+  } catch (error) {
+    console.error("Error connecting to Database:", error.message);
+  }
 }
-
 
 module.exports = ConnectToDatabase;
